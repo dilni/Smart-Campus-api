ab00ffdc973fcb2972b455d208aa072903fbc8e2
dilni <rohansiwije@gmail.com>
2026-04-23 23:37:33 +0530

    fix: add missing imports to ConflictExceptionMapper.

--- src/main/java/com/smartcampus/exception/ConflictExceptionMapper.java
+++ src/main/java/com/smartcampus/exception/ConflictExceptionMapper.java
@@ -1,6 +1,7 @@
 package com.smartcampus.exception;
 
-
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
 
